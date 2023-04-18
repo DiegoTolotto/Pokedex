2ac6fc1fc5c02286263a0c7082e332315d370ac9
Diego Tolotto <diego.tolotto8@gmail>
2023-04-18 10:44:22 -0300

    Começando a definir os primeiros passos para procurar os pokemons na API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
+import axios from 'axios';
 import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
 
     const [pokemon, setPokemon] = useState([]);
+    const [pokemonData, setPokemonData] = ([])
 
-    const getPokemon = () => {
-      const array = []
+    const getPokemon = async () => {
+      const toArray = []
 
-      
+      try {
+        const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        const res = await axios.get(url)
+
+        toArray.push(res)
+        setPokemonData(toArray)
+      } catch (error) {
+        console.log(error)
+      }
+
     }
 
   return (
     <div className="App">
-
+      <form onChange={handleSubmit}>
+        <label>
+          <input onChange={handleChange}></input>
+        </label>
+      </form>
     </div>
   );
 }
