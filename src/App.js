c960d462068c19115a5def1c8cd8489a1965e6e1
Diego Tolotto <diego.tolotto8@gmail>
2023-04-18 10:53:44 -0300

    funções para enviar dados a api

--- src/App.js
+++ src/App.js
@@ -14,18 +14,28 @@ function App() {
         const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
         const res = await axios.get(url)
 
-        toArray.push(res)
+        toArray.push(res.data)
         setPokemonData(toArray)
       } catch (error) {
         console.log(error)
       }
 
     }
+    // Função que define os dados a APi;
+    const handleSubmit = (e) => {
+      setPokemon(e.target.value.toLowerCase())
+    }
+    // Função que envias os dados para a APi;
+    const handleChange = (e) => {
+      e.preventDefault();
+      getPokemon();
+    }
 
   return (
     <div className="App">
       <form onChange={handleSubmit}>
         <label>
+          {/* Input que vai definir o pokemon que estamos procurando */}
           <input onChange={handleChange}></input>
         </label>
       </form>
